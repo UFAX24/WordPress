934423fa8aabcd01b3e613e192de48fa4cbaa256
lottostock <lottostock.pnck@gmail.com>
2021-08-08 01:37:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
 <?php
-	try{
-		if (!file_exists('anti_ddos/start.php'))
-			throw new Exception ('anti_ddos/start.php does not exist');
-		else
-			require_once('anti_ddos/start.php'); 
-	} 
-	//CATCH the exception if something goes wrong.
-	catch (Exception $ex) {
-
-	
-		echo $ex->getMessage();
-	}
-?>
-
-<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -26,7 +11,20 @@
  *
  * @var bool
  */
+try{
+	if (!file_exists('anti_ddos/start.php'))
+		throw new Exception ('anti_ddos/start.php does not exist');
+	else
+		require_once('anti_ddos/start.php'); 
+} 
+//CATCH the exception if something goes wrong.
+catch (Exception $ex) {
+
+
+	echo $ex->getMessage();
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 require __DIR__ . '/wp-blog-header.php';
+}
+?>
